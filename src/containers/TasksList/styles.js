6d62adaf7b48cabe1cf8c5f5a97e5213906a6a48
Ignacio Caldumbide <ignacio.caldumbide@accenture.com>
2022-01-25 00:42:10 -0300

    modifies task lists container & styles

--- src/containers/TasksList/styles.js
+++ src/containers/TasksList/styles.js
@@ -2,12 +2,9 @@ import { Box, Typography } from '@mui/material';
 import styled from '@emotion/styled';
 
 export const Wrapper = styled(Box)`
-    box-shadow: rgba(17, 17, 26, 0.1) 0px 1px 0px, rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 48px;
-    width: 40%;
-    padding: 20px;
-    border-radius: 10px;
-    z-index: 0;
     display: flex;
+    width: 80%;
+    padding: 20px;
     flex-direction: column;
 `;
 
@@ -35,4 +32,16 @@ export const TextBox = ({onSubmit, ...props}) => {
 
 export const DeleteButton = styled(Typography)`
     cursor: pointer;
+`;
+
+export const InputWrapper = styled(Box)`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const AddTaskInputWrapper = styled(Box)`
+    display: flex;
+    align-items: center;
+    gap: 5px;
 `;
